743bbda207bc4eeae3cb6a9f2d5aac0ce9b9d1fa
ConnieMLavio <conniemlavio@users.noreply.github.com>
2021-10-05 05:42:33 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" List - simple list """
+
+def main():
+    # create a simple list of 3-letter animal names
+    animlist1 = ["dog","cat","eel","ant","bug"]
+
+    # display animlist1
+    print(animlist1)
+
+main ()
